55f8a942256929b0a6ee878abdbecd57a37cf4b9
Saikat Samanta <iamsaikatsamanta@gmail.com>
2020-11-25 18:15:19 +0530

    Added ViewSet

--- profile_api/views.py
+++ profile_api/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from profile_api import serializers
+from rest_framework.viewsets import ViewSet
 
 class HelloApiView(APIView):
     """Test Api View"""
@@ -21,3 +22,29 @@ class HelloApiView(APIView):
             return Response({"code": 0, "result": f'Hi {name}'})
 
         return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+
+class HelloViewSet(ViewSet):
+    """API View Set"""
+    serializer_class = serializers.HelloSerializer
+    def list(self, request):
+        return Response({"code": 0, "result": "Hello List"})
+
+
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            return Response({"code": 0, "result": f'Hi {name}'})
+        return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        return Response({"code": 0, "result": "Hello GET BY ID"})
+
+    def update(self, request, pk=None):
+        return Response({"code": 0, "result": "Hello PUT"})
+
+    def partial_update(self, request, pk=None):
+        return Response({"code": 0, "result": "Hello PATCH"})
+
+    def destroy(self, request, pk=None):
+        return Response({"code": 0, "result": "Delete"})
